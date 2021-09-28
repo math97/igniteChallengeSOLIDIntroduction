efc0b35525be4c02b2709f287be2441ae0f60d3b
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2021-09-28 09:23:01 -0300

    ListAllUsers UseCase passed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,9 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user || !user.admin) throw new Error("User not a admin");
+    return this.usersRepository.list();
   }
 }
 
